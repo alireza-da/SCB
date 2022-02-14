312a824ef8d2f680b63607a018ebfe54caa3e3b4
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2022-02-14 20:10:38 +0330

    gunicorn proc requirements.txt

--- credentials.py
+++ credentials.py
@@ -1,4 +1,5 @@
 import os
 bot_token = os.environ['bot_token']
+db_url = os.environ['DATABASE_URL']
 application_id = "936315608692318289"
 public_key = "1ef3916d9fccb0719cb73314c18f5d000117959346e6c73b9f7cd32e596a5b67"
