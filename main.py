d5e832972ccaebdb6478c782f8a619a40271b7e4
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2022-02-24 04:14:49 +0330

    lyrics with arg

--- main.py
+++ main.py
@@ -302,11 +302,9 @@ async def lyrics(ctx, filename):
     if len(lyrics) < 2000:
         await ctx.channel.send(f"```{lyrics}```")
     else:
-        sections = [lyrics[i:i + 1998] for i in range(0, len(lyrics), 1998)]
+        sections = [lyrics[i:i + 1500] for i in range(0, len(lyrics), 1500)]
         for section in sections:
             await ctx.channel.send(f"```{section}```")
-
-
 
 keep_alive()
 client.run(credentials.bot_token)
